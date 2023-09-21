9af7d2901ce3b620a91be81b731442977de1e561
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-09-21 11:16:46 +0300

    main_delete.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+# import models
 from models.base_model import BaseModel, Base
-from models import storage_type, storage
 from models.city import City
+from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage_type, storage
 
 
 class State(BaseModel, Base):
