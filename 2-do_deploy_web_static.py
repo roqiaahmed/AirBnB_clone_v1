43a225440216c8f18a4c2fa81232ed09a55bc50f
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-05 16:01:33 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,19 +28,19 @@ def do_deploy(archive_path):
     '''
     if not os.path.exists(archive_path):
         return False
-    archive_name = archive_path.split("/")[1]
-    releases_path = '/data/web_static/releases' + archive_name[:-4]
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        put("{}, /tmp/".format(archive_path))
+        put(archive_path, '/tmp/')
         run('mkdir -p {}'.format(releases_path))
-        run('tar -xvf /tmp/{} -C {}'.format(archive_name, releases_path))
-        run('rm -r /tmp/{}'.format(archive_name))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
         run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
         run('rm -rf {}/web_static'.format(releases_path))
         run('rm -rf /data/web_static/current')
         run('ln -s {} /data/web_static/current'.format(releases_path))
         print('New version deployed!')
         return True
-
     except:
         return False
