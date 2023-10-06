284cacfd5f3017ef83e189374018df2667b7ed30
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-06 10:59:42 +0300

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,10 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
+SRC="/etc/nginx/sites-available/default"
+STATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee "/data/web_static/releases/test/index.html"
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
