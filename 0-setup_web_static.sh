16073dd5e147b627bae84d84a8f9b739d405b8c4
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-06 09:38:53 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,29 @@
 # sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
+
+# #!/usr/bin/env bash
+# # sets up the web servers for the deployment of web_static
+
+# sudo apt-get -y update
+# sudo apt-get -y install nginx
+# sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+# echo "<html>
+#   <head>
+#   </head>
+#   <body>
+#     Holberton School
+#   </body>
+# </html>" > /data/web_static/releases/test/index.html
+# sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# sudo chown -R ubuntu:ubuntu /data/
+# sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+# sudo service nginx restart
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,9 @@ Fabric script that Generates a .tgz archive
 from fabric.api import *
 from datetime import datetime
 import os
-env.hosts = ['	54.157.165.199', '54.237.14.178']
+env.hosts = ['54.157.165.199', '54.237.14.178']
 env.user = 'ubuntu'
+
 def do_pack():
     '''
     Generates a tgz archive from the
